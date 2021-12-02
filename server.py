c892a1f307f746992f007848864c4e0b50199ec3
Dedaniya08 <dedaniya08@gmail.com>
2021-12-02 20:00:12 +0530

    New file has been updated

---
+++ server.py
@@ -0,0 +1,31 @@
+"""
+Main module of the server file
+"""
+
+# 3rd party moudles
+from flask import render_template
+import connexion
+import config
+
+
+# create the application instance
+#app = connexion.App(__name__, specification_dir="./")
+app = config.connex_app
+# Cead the swagger.yml file to configure the endpoints
+app.add_api("swagger.yml")
+
+
+# Create a URL route in our application for "/"
+@app.route("/")
+def home():
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
